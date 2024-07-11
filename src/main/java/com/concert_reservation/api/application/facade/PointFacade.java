47147da5f37e94793f236bfaad9fb054c1746f77
hyejin0662 <hyejin0776@gmail.com>
2024-07-12 05:35:11 +0900

    feat. facade 구현

--- src/main/java/com/concert_reservation/api/application/facade/PointFacade.java
+++ src/main/java/com/concert_reservation/api/application/facade/PointFacade.java
@@ -2,41 +2,37 @@ package com.concert_reservation.api.application.facade;
 
 import org.springframework.stereotype.Component;
 
+import com.concert_reservation.api.application.dto.request.PaymentRequest;
 import com.concert_reservation.api.application.dto.request.PointRequest;
+import com.concert_reservation.api.application.dto.response.PaymentResponse;
 import com.concert_reservation.api.application.dto.response.PointResponse;
 import com.concert_reservation.api.business.model.dto.command.PointCommand;
 import com.concert_reservation.api.business.model.dto.info.PointInfo;
-import com.concert_reservation.api.business.service.impl.PointServiceImpl;
+import com.concert_reservation.api.business.service.PointService;
 
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class PointFacade {
-    private final PointServiceImpl pointServiceImpl;
+    private final PointService pointService;
 
-    // public PointResponse processPayment(PointRequest pointRequest) {
-    //     return PointResponse.from(pointServiceImpl.processPayment(pointServiceImpl));
-    // }
 
-    public PointResponse createPoint(PointRequest pointRequest) {
+
+    public PointResponse chargePoint(PointRequest pointRequest) {
         PointCommand pointCommand = new PointCommand(null, pointRequest.getUserId(), pointRequest.getBalance(), pointRequest.getAmount(), pointRequest.getPaymentTime(), pointRequest.getPaymentMethod());
-        PointInfo pointInfo = pointServiceImpl.createPoint(pointCommand);
+        PointInfo pointInfo = pointService.chargePoint(pointCommand);
         return PointResponse.from(pointInfo);
     }
 
     public PointResponse getPoint(Long pointId) {
-        PointInfo pointInfo = pointServiceImpl.getPoint(pointId);
+        PointInfo pointInfo = pointService.getPoint(pointId);
         return PointResponse.from(pointInfo);
     }
 
-    public PointResponse updatePoint(Long pointId, PointRequest pointRequest) {
-        PointCommand pointCommand = new PointCommand(pointId, pointRequest.getUserId(), pointRequest.getBalance(), pointRequest.getAmount(), pointRequest.getPaymentTime(), pointRequest.getPaymentMethod());
-        PointInfo pointInfo = pointServiceImpl.updatePoint(pointId, pointCommand);
-        return PointResponse.from(pointInfo);
+    public PointResponse deductPoint(PointRequest pointRequest) {
+        return PointResponse.from(pointService.deductPoint(pointRequest.toCommand()));
     }
 
-    public void deletePoint(Long pointId) {
-        pointServiceImpl.deletePoint(pointId);
-    }
+
 }
--- src/main/java/com/concert_reservation/api/application/facade/UserFacade.java
+++ src/main/java/com/concert_reservation/api/application/facade/UserFacade.java
@@ -2,62 +2,52 @@ package com.concert_reservation.api.application.facade;
 
 import org.springframework.stereotype.Component;
 
-import com.concert_reservation.api.application.dto.request.TokenRequest;
 import com.concert_reservation.api.application.dto.request.UserRequest;
-import com.concert_reservation.api.application.dto.response.TokenResponse;
 import com.concert_reservation.api.application.dto.response.UserResponse;
 import com.concert_reservation.api.business.model.dto.command.UserCommand;
 import com.concert_reservation.api.business.model.dto.info.UserInfo;
-import com.concert_reservation.api.business.service.impl.TokenServiceImpl;
-import com.concert_reservation.api.business.service.impl.UserServiceImpl;
+import com.concert_reservation.api.business.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class UserFacade {
-    private final UserServiceImpl userServiceImpl;
-    private final TokenServiceImpl tokenServiceImpl;
+	private final UserService userServiceImpl;
 
-    public Long getUserBalance(String userId) {
-        return userServiceImpl.getUserBalance(userId);
-    }
 
-    public Long chargeUserBalance(String userId, Long amount) {
-        // return userServiceImpl.chargeUserBalance(userId, amount);
-    return null;
-    }
+	public UserResponse createUser(UserRequest userRequest) {
+		UserCommand userCommand = UserCommand.builder()
+			.userId(userRequest.getUserId())
+			.name(userRequest.getName())
+			.email(userRequest.getEmail())
+			.phoneNumber(userRequest.getPhoneNumber())
+			.balance(userRequest.getBalance())
+			.build();
+		UserInfo userInfo = userServiceImpl.createUser(userCommand);
+		return UserResponse.from(userInfo);
+	}
 
-        public UserResponse createUser(UserRequest userRequest) {
-            UserCommand userCommand = UserCommand.builder()
-                .userId(userRequest.getUserId())
-                .name(userRequest.getName())
-                .email(userRequest.getEmail())
-                .phoneNumber(userRequest.getPhoneNumber())
-                .balance(userRequest.getBalance())
-                .build();
-            UserInfo userInfo = userServiceImpl.createUser(userCommand);
-            return UserResponse.from(userInfo);
-        }
+	public UserResponse getUser(String userId) {
+		UserInfo userInfo = userServiceImpl.getUser(userId);
+		return UserResponse.from(userInfo);
+	}
 
-        public UserResponse getUser(String userId) {
-            UserInfo userInfo = userServiceImpl.getUser(userId);
-            return UserResponse.from(userInfo);
-        }
+	public UserResponse updateUser(String userId, UserRequest userRequest) {
+		UserCommand userCommand = UserCommand.builder()
+			.userId(userId)
+			.name(userRequest.getName())
+			.email(userRequest.getEmail())
+			.phoneNumber(userRequest.getPhoneNumber())
+			.balance(userRequest.getBalance())
+			.build();
+		UserInfo userInfo = userServiceImpl.updateUser(userId, userCommand);
+		return UserResponse.from(userInfo);
+	}
 
-        public UserResponse updateUser(String userId, UserRequest userRequest) {
-            UserCommand userCommand = UserCommand.builder()
-                .userId(userId)
-                .name(userRequest.getName())
-                .email(userRequest.getEmail())
-                .phoneNumber(userRequest.getPhoneNumber())
-                .balance(userRequest.getBalance())
-                .build();
-            UserInfo userInfo = userServiceImpl.updateUser(userId, userCommand);
-            return UserResponse.from(userInfo);
-        }
+	public void deleteUser(String userId) {
+		userServiceImpl.deleteUser(userId);
+	}
 
-        public void deleteUser(String userId) {
-            userServiceImpl.deleteUser(userId);
-        }
+
 }
