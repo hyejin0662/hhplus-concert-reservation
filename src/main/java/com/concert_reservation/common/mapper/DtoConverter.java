7ea7b9909d792ec68603c35c139935d3db09b07b
hyejin0662 <hyejin0776@gmail.com>
2024-08-16 00:31:21 +0900

    feat: mapper 수정 및 topic enum 추가

--- src/main/java/com/concert_reservation/common/mapper/DtoConverter.java
+++ src/main/java/com/concert_reservation/common/mapper/DtoConverter.java
@@ -2,19 +2,32 @@ package com.concert_reservation.common.mapper;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 public class DtoConverter {
     private static final ObjectMapper objectMapper;
 
     static {
         objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new Jdk8Module());
+        objectMapper.registerModule(new ParameterNamesModule());
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        objectMapper.registerModule(javaTimeModule);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
     }
 
     public static <T> T convert(Object source, Class<T> targetClass) {
         return objectMapper.convertValue(source, targetClass);
+    }
+
+    public static ObjectMapper getObjectMapper(){
+        return objectMapper;
     }
 }
