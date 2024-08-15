5829e0a9943e45d1fc5b461045dfbf5cdb407d00
hyejin0662 <hyejin0776@gmail.com>
2024-08-16 00:32:00 +0900

    feat: config 파일 추가 및 ConcertReservationApplication 파일 수정

--- src/main/java/com/concert_reservation/ConcertReservationApplication.java
+++ src/main/java/com/concert_reservation/ConcertReservationApplication.java
@@ -2,10 +2,12 @@ package com.concert_reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing
 public class ConcertReservationApplication {
 
 	public static void main(String[] args) {
